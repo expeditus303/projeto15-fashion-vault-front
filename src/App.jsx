903d7266144c558670246079d080a5f6c720c013
guilhermcos <guigc0408@gmail.com>
2023-04-27 14:48:38 -0300

    add pages changes on footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer/Footer";
+import ShopPage from "./pages/ShopPage";
+import React, { createContext, useState } from "react";
 
 export default function App() {
+  const [headerData, setHeaderData] = useState();
+
+  const HeaderData = React.createContext();
+
   return (
     <PagesContainer>
       <BrowserRouter>
         <Routes>
           <Route path="/home" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
-      <Footer />
     </PagesContainer>
   );
 }
